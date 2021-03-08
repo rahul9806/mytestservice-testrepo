53f1eba6436318d954b278c74bea219be1ac0865
abc <abc@example.com>
2021-03-08 12:54:35 +0530

    first commit

---
+++ src/main/java/com/icici/GreetingClass.java
@@ -0,0 +1,14 @@
+package com.icici;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingClass {
+	
+	public static void main(String[] args) {
+		
+	SpringApplication.run(GreetingClass.class, args);
+	}
+
+}
